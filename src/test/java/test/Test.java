6c256430048299f241d39a1388f70b1fb98eae4f
Quentin Plaud <qplaud@gmail.com>
2021-07-26 15:59:52 +0200

    Debug with console in list

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import metier.Console;
 import metier.Jeu;
 
@@ -7,17 +10,24 @@ public class Test {
 
 	public static void main(String[] args) {
 		
+		List <Console> consoles = new ArrayList();
+		
 		Console ds = new Console("DS");
 		Console wii = new Console("WII");
 		Console swtch = new Console("Switch");
-
 		
-		Jeu j1 = new Jeu("Mario_Kart", ds);
-		Jeu j2 = new Jeu("Pokemon", wii);
-		Jeu j3 = new Jeu("Zelda", swtch);
-		Jeu j4 = new Jeu("Smash Bros", swtch);
-		Jeu j5 = new Jeu("Metroid", ds);
-
+		consoles.add(ds);
+		consoles.add(wii);
+		consoles.add(swtch);		
+		
+		Jeu j1 = new Jeu("Mario_Kart", (List) consoles.get(0));
+		Jeu j2 = new Jeu("Pokemon", (List) consoles.get(1));
+		Jeu j3 = new Jeu("Zelda", (List) consoles.get(2));
+		Jeu j4 = new Jeu("Smash Bros", (List) consoles.get(2));
+		Jeu j5 = new Jeu("Metroid", (List) consoles.get(0));
+		
+		Client c1 = new Client("Testi", "Testou", )
+			
 	}
 
 }
